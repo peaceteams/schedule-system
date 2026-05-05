c340ba1ebd7fb072713566ca4ca2a779f11b5f4a
kanaderu0 <realuse@outlook.jp>
2026-05-05 15:47:57 +0900

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,35 +1,33 @@
 import { NextResponse } from "next/server";
-import { jwtVerify } from "jose";
+import jwt from "jsonwebtoken";
 
-export async function middleware(req) {
-    const url = req.nextUrl.clone();
-    const token = req.cookies.get("admin_session")?.value;
+export function middleware(req) {
+  const url = req.nextUrl.clone();
+  const token = req.cookies.get("admin_session")?.value;
 
-    if (url.pathname.startsWith("/admin")) {
+  if (url.pathname.startsWith("/admin")) {
     if (url.pathname.startsWith("/admin/login")) {
-        return NextResponse.next();
+      return NextResponse.next();
     }
 
     if (!token) {
-        url.pathname = "/admin/login";
-        return NextResponse.redirect(url);
+      url.pathname = "/admin/login";
+      return NextResponse.redirect(url);
     }
 
     try {
-        const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-        await jwtVerify(token, secret);
-
-        return NextResponse.next();
+      jwt.verify(token, process.env.JWT_SECRET);
+      return NextResponse.next();
     } catch (err) {
-        url.pathname = "/admin/login";
-        return NextResponse.redirect(url);
+      url.pathname = "/admin/login";
+      return NextResponse.redirect(url);
     }
-    }
+  }
 
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/admin/:path*"],
+  matcher: ["/admin/:path*"],
+  runtime: "nodejs",
 };
