91bdd967d18f017520e486463bcd39fb09f1509f
kanaderu0 <realuse@outlook.jp>
2026-05-06 02:00:18 +0900

    Update checkVerified.js

--- pages/api/checkVerified.js
+++ pages/api/checkVerified.js
@@ -9,5 +9,5 @@ export default async function handler(req, res) {
     return res.status(404).json({ verified: false });
   }
 
-  return res.status(200).json({ verified: admin.is_verified === 1 });
+  return res.status(200).json({ verified: admin.is_verified === true });
 }
